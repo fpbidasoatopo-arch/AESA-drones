ca5e98b19adcd744f9b240a3b7561c43b08a015f
fpbidasoatopo-arch <fpbidasoatopo@gmail.com>
2026-09-18 10:25:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "altura120-v1";
+const CACHE = "altura120-v2";
 const FICHEROS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
